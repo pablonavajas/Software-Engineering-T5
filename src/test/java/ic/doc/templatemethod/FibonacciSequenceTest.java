9929a6f5e9a7b562175e104f63359ecfdc4476b8
Pablo Navajas Helguero <P.Navajas-Helguero@warwick.ac.uk>
2020-02-16 10:56:44 +0100

    PS - Fix tests to match style checks

--- src/test/java/ic/doc/templatemethod/FibonacciSequenceTest.java
+++ src/test/java/ic/doc/templatemethod/FibonacciSequenceTest.java
@@ -1,12 +1,12 @@
 package ic.doc.templatemethod;
 
+import org.junit.Test;
+
 import static ic.doc.matchers.IterableBeginsWith.beginsWith;
 import static org.hamcrest.CoreMatchers.containsString;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.fail;
-
-import org.junit.Test;
 
 public class FibonacciSequenceTest {
 
