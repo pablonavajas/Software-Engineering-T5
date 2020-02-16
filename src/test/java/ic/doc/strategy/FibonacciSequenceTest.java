e8eb7367552202744aba3e2865f0484fba554328
Pablo Navajas Helguero <P.Navajas-Helguero@warwick.ac.uk>
2020-02-16 11:53:25 +0100

    PS - fixture of test imports v.2

--- src/test/java/ic/doc/strategy/FibonacciSequenceTest.java
+++ src/test/java/ic/doc/strategy/FibonacciSequenceTest.java
@@ -1,8 +1,8 @@
 package ic.doc.strategy;
 
 import static ic.doc.matchers.IterableBeginsWith.beginsWith;
-import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.fail;
 
